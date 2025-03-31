cae82042bafd7f70997b6a7abbea130865859993
diego vidal <diego27796gagan@gmail.com>
2025-03-30 23:32:13 -0600

    create App.jsx component, migrated from old version

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from 'react';
+import IntroductionSection from './IntroductionSection';
+import NavigationBar from './NavigationBar';
+import PortfolioSection from './PortfolioSection';
+import AboutSection from './AboutSection';
+import ContactForm from './ContactForm';
+
+
+function App() {
+  const [activeSection, setActiveSection] = useState('introduction'); 
+  // ['introductionRef', 'portfolioRef', 'aboutRef', 'contactRef'
+  const introductionRef = useRef(null);
+  const portfolioRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
+
+  const scrollToSection = (refName) => {
+    const sectionRef = {
+      introductionRef,
+      portfolioRef,
+      aboutRef,
+      contactRef,
+    }[refName];
+
+    if (sectionRef) {
+       sectionRef.current.scrollIntoView({ behavior: 'smooth' })
+        setActiveSection(refName);
+    }
+  };
+
+  return (
+    <>
+      <header className='font-poppins'>
+        <NavigationBar scrollToSection={scrollToSection} activeSection={activeSection}/>
+      </header>
+      <main className='bg-[#F4F5F7] font-poppins'>
+        <IntroductionSection sectionRef={introductionRef} isActive={activeSection === 'introductionRef'}/>
+        <PortfolioSection sectionRef={portfolioRef} isActive={activeSection==='portfolioRef'}/>
+        <AboutSection sectionRef={ aboutRef } isActive={activeSection==='aboutRef'}/>
+        <ContactForm sectionRef={ contactRef } isActive={activeSection==='contactRef'}/>
+      </main>
+    </>
+  );
+}
+
+export default App;
