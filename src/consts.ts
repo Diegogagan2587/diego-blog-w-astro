3cee8ee330201e40258e2bf26dff4b2f5dd83fe2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-04-25 00:52:13 -0600

    chore(const.ts): updates site title and sitle description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'DVLoper-z';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Diego Vidal | Full-Stack Ruby On Rails Developer';
+export const SITE_DESCRIPTION = 'Building business software, internal tools, SaaS products, and scalable web apps.';
