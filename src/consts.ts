55ed87d4e394eff5c6a3612b8485a8694b74e2c7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-04-25 01:12:22 -0600

    chore: udpates site title and site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Diego Vidal | Full-Stack Ruby On Rails Developer';
-export const SITE_DESCRIPTION = 'Building business software, internal tools, SaaS products, and scalable web apps.';
+export const SITE_TITLE = 'Diego Vidal | Ruby on Rails Developer for Business Software';
+export const SITE_DESCRIPTION = 'Ruby on Rails developer building internal tools, dashboards, SaaS products, APIs, and scalable web apps. Available for remote opportunities and custom software projects.';
