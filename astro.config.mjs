856b90d86ec525f0b15708df73db6dd2ed7f565f
Diego Vidal Lopez <diego27796gagan@gmail.com>
2026-04-25 20:18:02 -0600

    config(Astro): install brainDb integration and enable wiki-link features for markdown notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,45 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import remarkWikiLink from '@braindb/remark-wiki-link'
+import { brainDbAstro } from '@braindb/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dvloper-z.com/',
-  integrations: [mdx(), sitemap(), react()],
-
+  integrations: [mdx(), sitemap(), react(), brainDbAstro({ remarkWikiLink: false})],
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkWikiLink,
+        {
+          linkTemplate: ({ slug, alias }) => {
+            const [slugWithoutAnchor, anchor] = slug.split("#");
+            
+            // Convert slug to URL - matches Astro's [...slug] routing
+            const url = `/${slugWithoutAnchor}${anchor ? `#${anchor}` : ''}`;
+            
+            return {
+              hName: "a",
+              hProperties: {
+                href: url,
+                class: "wiki-link",
+              },
+              hChildren: [
+                {
+                  type: "text",
+                  value: alias || slugWithoutAnchor,
+                },
+              ],
+            };
+          },
+        },
+      ],
+    ]
+  },
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
